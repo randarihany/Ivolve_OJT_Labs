c5b7cbf8557b2bc00e1b117d695ea34c81d39b33
randarihany <94257491+randarihany@users.noreply.github.com>
2025-03-09 03:23:42 +0200

    Create variables.tf

---
+++ Terraform_Lab19/code/modules/server/variables.tf
@@ -0,0 +1,24 @@
+variable "vpc_id" {
+  description = "The ID of the VPC"
+  type        = string
+}
+
+variable "subnet_id" {
+  description = "The ID of the subnet"
+  type        = string
+}
+
+variable "ami_id" {
+  description = "The AMI ID for the EC2 instance"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "The instance type for the EC2 instance"
+  type        = string
+}
+
+variable "instance_name" {
+  description = "Name of the EC2 instance"
+  type        = string
+}
